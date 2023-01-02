a04ed365089f48b9b45242c2fc0d13538e8d4872
ryanHUNGry <ryhung@ucsd.edu>
2023-01-01 21:38:51 -0800

    Webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,11 @@
-import _ from 'lodash'    
+import _ from 'lodash';
+import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = myName("cody")
   
     return element;
   }
